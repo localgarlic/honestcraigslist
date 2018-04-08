475dc4f4877e78070e2faa8cb80e2385da14791d
Jeff Walsh <rlxrlps@gmail.com>
2018-04-08 16:52:20 -0700

    add disable and reload

--- popup.js
+++ popup.js
@@ -1,11 +1,24 @@
 function toggle() {
     console.log("toggle");
-    chrome.management.getSelf(result => {
-        console.log(result.id);
-        chrome.management.setEnabled(result.id, !result.enabled, () => {
-            console.log("done");
-        });    
-    });
+    // chrome.management.getSelf(result => {
+    //     console.log(result.id);
+    //     chrome.management.setEnabled(result.id, !result.enabled, () => {
+    //         console.log("done");
+    //     });    
+    // });
+
+    chrome.storage.local.get("hcl", function(result) {
+        console.log(result.hcl);
+        if (result.hcl == "true") {
+            chrome.storage.local.set({"hcl": "false"}, function() {});
+        }
+        
+        if (result.hcl == "false") {
+            chrome.storage.local.set({"hcl": "true"}, function() {});
+        }
+
+        chrome.tabs.reload();
+      });
 }
 
 const ele = document.getElementById("toggle");
